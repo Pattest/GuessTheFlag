888bd96ef505e136ef148e453cc644e70e994e8a
Pattest <baptistecadoux@gmail.com>
2021-09-02 14:17:28 +0200

    feat(Day 24): export Image into new struct FlagImage

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var name: String
+
+    var body: some View {
+        Image(name)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            .shadow(color: .black, radius: 2)
+    }
+}
+
 struct ContentView: View {
     @State private var countries = [
         "Estonia",
@@ -48,11 +60,7 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                        FlagImage(name: self.countries[number])
                     }
                 }
 
